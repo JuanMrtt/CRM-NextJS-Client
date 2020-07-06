ddb18a5d351737e71b379262de4af947671d4d65
Juan Antonio Martin <juanantoniomartinb@gmail.com>
2020-07-06 18:37:20 +0200

    Create Project and pages, router and aside

---
+++ pages/nuevacuenta.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const nuevacuenta = () => {
+    return (
+        <>
+            <Layout>
+
+                <h1>
+                    nuevacuenta
+                </h1>
+            </Layout>
+        </>
+    )
+}
+
+export default nuevacuenta
